2f2aeef2e1d47dcd24639935ad02f3a916f50ce4
oddhus <huseby.oddmund@gmail.com>
2020-05-09 22:21:53 +0200

    new error msg format

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,23 +1,37 @@
-import React, { useState} from "react"
+import React, { useState, useRef } from "react"
 import { navigate } from "gatsby"
 import { useForm } from "react-hook-form"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import firebase from "gatsby-plugin-firebase"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import {Form, Input, Button} from '../components/Common/Index'
 
+const ErrorMsg = styled.p`
+  color: #707070;
+  font-size: small;
+  font-style: italic;
+  margin-top: -6px;
+  margin-bottom: 8px;
+  padding: 0;
+  }
+`
+
 const Register = () => {
-  const { register, handleSubmit, errors } = useForm()
+  const { register, handleSubmit, errors, watch } = useForm()
+  const password = useRef({})
+  password.current = watch("password", "")
+
   const [user, initialising, error] = useAuthState(firebase.auth())
-  const [dbError, setDbError] = useState("")
+  const [dbError, setDbError] = useState(null)
 
   function onSubmit(data){
       firebase.auth().createUserWithEmailAndPassword(
         data.email,
         data.password
       ).then(user => {
-        firebase.firestore().collection('users').doc(user.user.uid).set({
+        firebase.firestore().collection('users').doc(!user.user.uid).set({
           username: data.username,
           email: data.email
         }).then(() => {
@@ -39,15 +53,35 @@ const Register = () => {
     navigate(`/`)
   }
 
+  console.log(errors)
+
   return (
   
     <Layout>
       <Form onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder="username" type="text" name="username" ref={register({ required: true })} />
-        <Input placeholder="email" type="email" name="email" ref={register({ required: true })} />
-        <Input placeholder="password" type="password" name="password" ref={register({ required: true })} />
-        <Input placeholder="confirm password" type="password" name="confirmPassword" ref={register({ required: true })} />
+        <Input placeholder="Username" type="text" name="username" ref={register({ required: "Username is required" })} />
+        {errors.username && <ErrorMsg>{errors.username.message}</ErrorMsg>}
+        <Input placeholder="Email" type="email" name="email" ref={register({ required: "Email is required" })} />
+        {errors.email && <ErrorMsg>{errors.email.message}</ErrorMsg>}
+        <Input placeholder="Password" type="password" name="password" 
+          ref={register({
+            required: "You must specify a password",
+            minLength: {
+              value: 6,
+              message: "Password must have at least 6 characters"
+            }
+          })}               
+        />
+        {errors.password && <ErrorMsg>{errors.password.message}</ErrorMsg>}
+        <Input placeholder="Confirm password" type="password" name="password_repeat" 
+          ref={register({
+            validate: value =>
+              value === password.current || "The passwords do not match"
+          })} 
+        />
+        {errors.password_repeat && <ErrorMsg>{errors.password_repeat.message}</ErrorMsg>}
 
+        {dbError && <ErrorMsg>{dbError}</ErrorMsg>}
         <Button type="submit" value="Register" block/>
       </Form>
     </Layout>
