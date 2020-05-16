6c731e22f6f04fa27df0ea51526f955e533aab2f
oddhus <huseby.oddmund@gmail.com>
2020-05-16 11:41:46 +0200

    changed import to require

--- src/pages/add-project.js
+++ src/pages/add-project.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { navigate } from "gatsby"
 import { useForm } from "react-hook-form"
 import { useAuthState } from 'react-firebase-hooks/auth';
