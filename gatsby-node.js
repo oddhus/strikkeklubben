3cbc1bb4206ce9ceea9e693e5ddefffb48f8969a
oddhus <huseby.oddmund@gmail.com>
2020-05-19 15:08:30 +0200

    removed orderBy

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,7 +17,6 @@ exports.onCreateNode = async ({ actions, createContentDigest, createNodeId }) =>
   const data = await admin.firestore()
       .collection('projects')
       .where('isPublic','==',true)
-      .orderBy('createdAt', 'desc')
       .get()
   
   const photos = data.docs.map(doc => {
